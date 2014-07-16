a25f95d3ef297e90c093b2e406601ae4f7c4aff9
Bharat Ponnaluri <bponnaluri@gmail.com>
2014-07-15 22:48:01 -0400

    Created server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,43 @@
-var express = require('express')
+var express = require('express');
+var storage = require('node-persist');
+
+var scores = 10; //The number of high scores that will be stored.
+
+storage.initSync();
+var data = {};
+
+for(var i=0;i<scores;i++){
+    var key=i+":Nobody";
+    data[key]="0";
+}
+storage.setItem('scores',data);
+
+
+var app = express();
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+app.get('/',function(req,res){
+    console.log("Connected to server");
+    res.send('Working');
+});
+
+app.get('/getScores',function(req,res){
+    storage.values(function(vals){
+        res.send(vals);
+    });
+});
+
+app.post('/addScores',function(req,res){
+    console.log("New data:"+req)
+    var scores=storage.getItem('scores');
+    scores["11"]="9999";
+    storage.setItem('scores',scores);
+    res.send(scores);
+});
+
+app.listen(3000);
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,55 @@
+window.server='http://localhost:3000/'; //The AJAX calls fail if the IP address is used.
+QUnit.asyncTest("Server test",function(){
+    expect(1);
+    $.ajax({
+        url:server,
+        type:'get',
+        success:function(data){
+            ok(data=="Working","Server is working");
+            start();
+        },
+        error:function(xhr,status,error){
+            console.log("Error:"+error);
+            ok(0);
+            start();
+        }
+    });
+});
 
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+QUnit.asyncTest("Get score test",function(){
+    expect(1);
+    $.ajax({
+        url:server+"getScores",
+        type:'get',
+        success:function(data){
+            ok(Object.keys(data).length==10);
+            start();
+        },
+        error:function(xhr,status,error){
+            console.log("Error:"+error);
+            ok(0);
+            start();
+        }
+    })
+});
+
+QUnit.asyncTest("Add score test",function(){
+    expect(1);
+    var scoreInfo={};
+    scoreInfo["player"]="test";
+    scoreInfo["score"]="999";
+    $.ajax({
+        url:server+"addScores",
+        type:'POST',
+        data:scoreInfo,
+        success:function(data){
+            ok(Object.keys(data).length==10);
+            start();
+        },
+        error:function(xhr,status,error){
+            console.log("Error:"+error);
+            ok(0);
+            start();
+        }
+    })
 });
